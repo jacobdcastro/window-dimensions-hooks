78ebf637f1824071f1299d6f9d0b656574f798f4
Jacob D. Castro <jdcastro.business@gmail.com>
2020-04-08 14:47:48 -0700

    include example build

---
+++ example/build/precache-manifest.192e4d6bab0d6c3e8340a1a352a401d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4eff5616dba8e25fcd3c78723bac43",
+    "url": "/window-dimensions-hooks/index.html"
+  },
+  {
+    "revision": "7ed6668f75da1eb2fb97",
+    "url": "/window-dimensions-hooks/static/css/main.24a6cd7c.chunk.css"
+  },
+  {
+    "revision": "c2e6842c2cf3b87bf6a4",
+    "url": "/window-dimensions-hooks/static/js/2.32a47a17.chunk.js"
+  },
+  {
+    "revision": "10fd6dbaa504ade6cb9e8cd86f21a077",
+    "url": "/window-dimensions-hooks/static/js/2.32a47a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed6668f75da1eb2fb97",
+    "url": "/window-dimensions-hooks/static/js/main.b0f207ff.chunk.js"
+  },
+  {
+    "revision": "11adb68e127484aca4ef",
+    "url": "/window-dimensions-hooks/static/js/runtime-main.35d4094c.js"
+  }
+]);
---
+++ example/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/window-dimensions-hooks/precache-manifest.192e4d6bab0d6c3e8340a1a352a401d6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/window-dimensions-hooks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
